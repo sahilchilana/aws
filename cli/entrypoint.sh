2e74cb5283e72f39427ef0f44a5e1585ff27f4df
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-11 19:26:26 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -28,3 +28,4 @@ aws stepfunctions create-state-machine --definition '{
               }
             }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/githubactiontesting-AddFunctionRole-828QDZ3VB97V"
 aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name test
+aws stepfunctions describe-execution --execution-arn arn:aws:states:us-east-1:670868576168:stateMachine:statemachine:test
