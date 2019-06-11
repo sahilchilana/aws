7a4da5f1a32be34520a4d8d7b1bc88d1248a6aa1
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-11 18:02:04 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -2,7 +2,6 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 export AWS_DEFAULT_REGION=us-east-1
-sleep 2m
 aws stepfunctions create-state-machine --definition '{
               "Comment": "Add two numbers and then subtact the result of add with another number",
               "StartAt": "AddNumbers",
