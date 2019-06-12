529b97e7fe556953e3d6f03095eef4f821d13c77
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-12 12:46:28 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -4,4 +4,4 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
 
 
-aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name test5
+aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name test6
