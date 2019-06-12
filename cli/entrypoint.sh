25a3cae3be8c555aca155b5aed9e98784dc2b3f8
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-12 13:06:42 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -3,5 +3,8 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
 
-
-aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name test7
+my_value=$(date)
+my_value=(${my_value// /_})
+aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name my_value
+sleep 30s
+aws stepfunctions describe-execution --execution-arn arn:aws:states:us-east-1:670868576168:execution:statemachine:my_value
