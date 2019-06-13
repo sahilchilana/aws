b757a8240af43bd34fc7781274143e6d5f055375
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-13 19:33:24 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -25,12 +25,13 @@ arn_value=$(aws stepfunctions create-state-machine --definition '{
                 } 
               }
               }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/lambda-vpc-role" 2>&1)
-#arn_value=(${arn_value// /_})
 if [[ $arn_value = *error* ]]; then
   error_value=$(echo $arn_value | grep error)
   arn_value=$(echo $error_value | cut -d "'" -f 2)
+else 
+  arn_value=$(echo $arn_value | jq .stateMachineArn | tr -d '"')
 fi
 echo $arn_value
-#execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
-#sleep 10s
-#aws stepfunctions describe-execution --execution-arn $execution_arn
+execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
+sleep 10s
+aws stepfunctions describe-execution --execution-arn $execution_arn
