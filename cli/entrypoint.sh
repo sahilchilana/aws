e8a200d30c26efc7f8858e9dc85d8d51b43f069b
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-13 16:06:44 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -5,28 +5,7 @@ export AWS_DEFAULT_REGION=us-east-1
 
 execution_name=$(date)
 execution_name=(${execution_name// /_})
-execution_name=(${execution_name//:/-})
-statemachine_arn=$(aws stepfunctions create-state-machine --definition '{
-              "Comment": "Add two numbers and then subtact the result of add with another number",
-              "StartAt": "AddNumbers",
-              "States": {
-                "AddNumbers": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-AddFunction-1KWO14YS9KT41",
-                  "Next": "SubtractNumbers"
-                },
-                  "SubtractNumbers": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-SubtractFunction-G5OHM6HZF29F",
-                  "Parameters":{
-                    "subtract from":40,
-                    "number1.$":"$"
-                  },
-                  "End": true
-                } 
-              }
-              }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/lambda-vpc-role" | jq .stateMachineArn | tr -d '"')
-  echo $arn_value           
-execution_arn=$(aws stepfunctions start-execution --state-machine $statemachine_arn --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
+execution_name=(${execution_name//:/-})         
+execution_arn=$(aws stepfunctions start-execution --state-machine arn:aws:states:us-east-1:670868576168:stateMachine:statemachine --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
 sleep 10s
 aws stepfunctions describe-execution --execution-arn $execution_arn
