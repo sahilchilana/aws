dcfc15af841c1af23003f74eac890bf981423cce
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-13 18:26:08 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -6,7 +6,7 @@ exec 2>&1
 execution_name=$(date)
 execution_name=(${execution_name// /_})
 execution_name=(${execution_name//:/-})  
-arn_value=$(aws stepfunctions create-state-machine --definition '{
+aws stepfunctions create-state-machine --definition '{
               "Comment": "Add two numbers and then subtact the result of add with another number",
               "StartAt": "AddNumbers",
               "States": {
@@ -25,8 +25,8 @@ arn_value=$(aws stepfunctions create-state-machine --definition '{
                   "End": true
                 } 
               }
-              }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/lambda-vpc-role" 2>&1| jq .stateMachineArn | tr -d '"')
-echo +++
+              }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/lambda-vpc-role"| jq .stateMachineArn | tr -d '"' >> file1
+arn_value=$(cat file1)
 echo $arn_value
 if [ "$?" -ne 0 ]; then
   error_value=$(echo $arn_value | grep error);
