c4b75dc07887d3c40f14e3c112823d1ccc603563
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-14 12:49:58 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -4,19 +4,26 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
 execution_name=$(date)
 execution_name=(${execution_name// /_})
-execution_name=(${execution_name//:/-})  
+execution_name=(${execution_name//:/-})
+all_functions=$(aws lambda list-functions --region us-east-1 --query 'Functions[?starts_with(FunctionName, `githubactiontesting`) == `true`].FunctionName' --output text)
+first_function_name=$(echo $all_functions | cut -d ' ' -f 1)
+second_function_name=$(echo $all_functions | cut -d ' ' -f 2)
+first_function_arn=$(aws lambda get-function --function-name $first_function_name)
+first_function_arn=$(echo $first_function_arn | jq .Configuration.FunctionArn)
+second_function_arn=$(aws lambda get-function --function-name $second_function_name)
+second_function_arn=$(echo $second_function_arn | jq .Configuration.FunctionArn)
 arn_value=$(aws stepfunctions create-state-machine --definition '{
               "Comment": "Add two numbers and then subtact the result of add with another number",
               "StartAt": "AddNumbers",
               "States": {
                 "AddNumbers": {
                   "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-AddFunction-1KWO14YS9KT41",
+                  "Resource": $first_function_arn,
                   "Next": "SubtractNumbers"
                 },
                   "SubtractNumbers": {
                   "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-SubtractFunction-G5OHM6HZF29F",
+                  "Resource": $second_function_arn,
                   "Parameters":{
                     "subtract from":40,
                     "number1.$":"$"
