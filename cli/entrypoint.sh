1d65b6f0ba935dc8af05013220a6617913d635d3
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-15 01:52:58 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -12,39 +12,71 @@ arn_value=$(echo $arn_value |jq '.StackResources[] | select(.ResourceType == "AW
 #2 integers
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 2_integers --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #2 strings
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $2_strings --input "{\"number1\":\"10\", \"number2\":\"20\"}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "FAILED" ]; then
+	echo "Test Passed"
+fi
 
 #2 negative numbers
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 2_negative_numbers --input "{\"number1\":-10, \"number2\":-20}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #1 positive and 1 negative integer
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 1_positive_and_1_negative --input "{\"number1\":10, \"number2\":-20}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #1 float and 1 integer
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 1_float_and_1_integer --input "{\"number1\":10.30, \"number2\":20}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #1 float and 1 negative integer
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 1_float_and_1_negative_integer --input "{\"number1\":10.30, \"number2\":-20}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #2 large integers
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 2_large_integers --input "{\"number1\":100000000000, \"number2\":2000000000000}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
 
 #2 large float
 execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name 2_large_float --input "{\"number1\":100000000000.30, \"number2\":2000000000000.40}"| jq .executionArn | tr -d '"')
 sleep 10s
-aws stepfunctions describe-execution --execution-arn $execution_arn
+outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
+output=$(echo $output| jq .status| tr -d '"')
+if [ "$output" == "SUCCEEDED" ]; then
+	echo "Test Passed"
+fi
