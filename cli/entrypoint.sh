f7011dc1399f7d18a844a832d1f0cd44c8dacb46
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-24 20:00:02 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -2,22 +2,13 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
-
-wget https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip
-sleep 1m
-unzip chromedriver_linux64.zip
-sleep 20s
-mv chromedriver /usr/bin/chromedriver
-chown root:root /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
-
-
-
-execution_name=$(date)
-execution_name=(${execution_name// /_})
-execution_name=(${execution_name//:/-})
-variable=$(python scrape1.py)
-echo $variable
+echo $HOME
+echo $GITHUB_WORKFLOW
+echo $GITHUB_ACTION
+echo $GITHUB_EVENT_PATH
+#execution_name=$(date)
+#execution_name=(${execution_name// /_})
+#execution_name=(${execution_name//:/-})
 #arn_value=$(aws cloudformation describe-stack-resources --stack-name githubactiontesting)
 #arn_value=$(echo $arn_value |jq '.StackResources[] | select(.ResourceType == "AWS::StepFunctions::StateMachine").PhysicalResourceId' | tr -d '"')
 #execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
