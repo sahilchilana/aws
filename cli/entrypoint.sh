e2e8682ac622555fc0a759a8b9c80c727682c4ff
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-24 12:39:34 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_IDmechanize._response.httperror_seek_wrapper: HTTP Error 404: Not Found
+
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
 #which python
 ls /usr/local/lib/python2.7/dist-packages
+
 #find / -name "cookielib.py"
 execution_name=$(date)
 execution_name=(${execution_name// /_})
