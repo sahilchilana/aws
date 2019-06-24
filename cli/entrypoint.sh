b49e0e89c19952f88b97201c69370fa14514d5a1
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-24 12:13:02 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -7,15 +7,4 @@ execution_name=$(date)
 execution_name=(${execution_name// /_})
 execution_name=(${execution_name//:/-})
 
-variable=$(python scrape1.py)
-echo $variable
-arn_value=$(aws cloudformation describe-stack-resources --stack-name githubactiontesting)
-arn_value=$(echo $arn_value |jq '.StackResources[] | select(.ResourceType == "AWS::StepFunctions::StateMachine").PhysicalResourceId' | tr -d '"')
-execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $execution_name --input "{\"number1\":10, \"number2\":20}"| jq .executionArn | tr -d '"')
-sleep 10s
-outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
-output=$(echo $output| jq .status| tr -d '"')
-echo "Output='$output'"
-if [ "$output" == "SUCCEEDED" ]; then
-	echo "Test Passed"
-fi
+
