d81af279f74d7ff0808bb0fe2e7404e548ef31fa
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-24 12:25:30 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -2,9 +2,9 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
-which python
+#which python
 ls /usr/local/lib/python2.7/site-packages
-find / -name "cookielib.py"
+#find / -name "cookielib.py"
 execution_name=$(date)
 execution_name=(${execution_name// /_})
 execution_name=(${execution_name//:/-})
